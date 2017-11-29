85108d0cf0e9d77dd6072a13d9dd783948140858
Nickcom4 <NickBakerWasHere@gmail.com>
2017-11-28 22:37:35 -0500

    Added second item to object

--- hello-world.js
+++ hello-world.js
@@ -1,5 +1,7 @@
 let myFirstObject = {
-  assignmentOne : 'Hello world!'
+  assignmentOne : 'Hello world!',
+  assignmentOneB: 'This is a random change to my test file'
 }
 
 console.log(myFirstObject.assignmentOne)
+console.log(myFirstObject.assignmentOneB)
